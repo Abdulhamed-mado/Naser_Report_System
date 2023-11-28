bdda47afd10a57630703d58861d1a5ab06cab88a
Abdulhamed-mado <abdulhamed.nafo@gmail.com>
2023-11-28 19:30:46 +0200

    logistic report added

--- resources/views/logisticReport.blade.php
+++ resources/views/logisticReport.blade.php
@@ -104,15 +104,15 @@
                                                 </td>
                                                 <td
                                                     class="px-2 py-2 text-xs text-gray-900 whitespace-nowrap text-center">
-                                                    {{ $$logustic->type }}
+                                                    {{ $logustic->type }}
                                                 </td>
                                                 <td
                                                     class="px-2 py-2 text-sm text-gray-900 whitespace-nowrap text-center">
-                                                    {{ $$logustic->team }}
+                                                    {{ $logustic->team }}
                                                 </td>
                                                 <td
                                                     class="px-2 py-2 text-sm text-gray-900 whitespace-nowrap text-center">
-                                                    {{ $$logustic->note }}
+                                                    {{ $logustic->note }}
                                                 </td>
 
                                             </tr>
